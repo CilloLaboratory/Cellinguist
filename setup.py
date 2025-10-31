5f1bf1e277ba4855359ab3001b0de43ec454d9c9
Anthony Cillo <arc85@pitt.edu>
2025-10-31 14:51:05 -0400

    setup file for package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cellinguist',
+    version='0.0.1.9000',
+    description='language of the cell',
+    license='MIT',
+    packages=['cellinguist'],
+    author='Anthony Cillo',
+    author_email='CilloLaboratory@gmail.com',
+    keywords=['scRNAseq','LLM','transformer','genomics','single-cell genomics'],
+    url='https://github.com/CilloLaboratory/Cellinguist'
+)
